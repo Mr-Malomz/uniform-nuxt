a7a9fa591fb82ce4e50a4e49648b6588d928b5f2
demola malomo <malomo.alaba@yahoo.com>
2022-08-10 01:35:41 +0100

    integrate uniform with nuxt 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import { defineNuxtConfig } from 'nuxt';
+import { ManifestV2 } from '@uniformdev/context';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
@@ -13,4 +14,11 @@ export default defineNuxtConfig({
     },
   },
   css: ['~/assets/css/tailwind.css'],
+  //add below section
+  modules: ['@uniformdev/uniform-nuxt'],
+  uniform: {
+    projectId: process.env.NUXT_ENV_PROJECT_ID,
+    readOnlyApiKey: process.env.NUXT_ENV_API_KEY,
+    manifest: {} as ManifestV2,
+  },
 });
